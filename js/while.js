cbc8181144088f712f993bcb1d0b403177eb5468
Jakira Dunson <jakiradunson@gmail.com>
2023-03-28 15:00:40 -0500

    dowhile

--- js/while.js
+++ js/while.js
@@ -1,15 +1,22 @@
-// var i = 2;
-//
-// while (i < 16) {
-//     i++;
-//     console.log((i * 2) );
-//
-//
-// }
+//while
 
-var i = 0;
+var i = 2;
 
 while (i <= 65536) {
     console.log(+i);
     i *= 2;
 }
+
+//do-while
+
+
+
+var allCones = Math.floor(Math.random() * 50) + 50;
+var i = 6;
+
+do {
+
+  var clientSale =  Math.floor(Math.random(i) * 5) + 1;
+    console.log('the amount of cones sold were: ' + clientSale);
+    i++;
+} while (i <6);
