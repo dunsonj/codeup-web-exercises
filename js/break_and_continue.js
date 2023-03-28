a25ceadc6c3c7ecd4719c3cfbaf0a87447ae183e
Jakira Dunson <jakiradunson@gmail.com>
2023-03-28 14:32:36 -0500

    while

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -16,14 +16,14 @@ var numberToStopAt = prompt("Pick an odd number between 1 and 50");
 console.log(numberToStopAt)
 
 for (var i = 1; i < 50; i++) {
-    if ((i % 2) != 0) {
+    if ((i % 2) == 0) {
        continue
   } else
-    if (i === numberToStopAt) {
+    if (i == numberToStopAt) {
         console.log("Yikes! Skipping number: " + i);
-
        continue;
-        // nothing after the break will get processed
-        console.log('You will never see this line.');
+
     }
+    console.log(i)
 }
+
---
+++ js/while.js
@@ -0,0 +1,8 @@
+var i = 1;
+
+while (i < 30) {
+    console.log(Math.pow(i , 2));
+    i++;
+
+
+}
