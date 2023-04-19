96553d6b35d30bab83aa244037e56fe1b73d2b2e
Jakira Dunson <jakiradunson@gmail.com>
2023-04-19 10:42:54 -0500

    jquery

---
+++ js/jquery_exercises.js
@@ -0,0 +1,33 @@
+"use strict";
+
+// $(function() {
+//
+//     alert( 'The DOM has finished loading!' );
+// });
+
+
+// //id Selectors
+// var classWork = $('#example').html()
+// console.log(classWork);
+// alert(classWork);
+//
+// var partTwo = $('#exampleTwo').html()
+// console.log(partTwo);
+// alert(partTwo);
+//
+//
+// //class selectors
+// $('.codeup').css('border' , '1px solid red' )
+// document.querySelector('.codeup')
+//
+// //element selectors
+// $('li').css('font-size', '20px')
+// $('h1').css('background-color', "yellow")
+// $('p').css('background-color', "yellow")
+// $('li').css('background-color', "yellow")
+//
+// var sho= $('h1').html()
+// alert(sho)
+
+//multiple selectors
+$('h1 , p , li').css('background-color', "yellow")
